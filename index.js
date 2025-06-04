1956a4bba5c794155a533086943bce556c3dde11
Ibnu Aqil Al Barohin <kalangkang1298@gmail.com>
2025-06-04 20:59:05 +0700

    add auto run init sql

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 const usersRoutes = require("./routes/users");
 const paymentsRoutes = require("./routes/payments");
 
+const fs = require("fs");
+const path = require("path");
+const db = require("./db");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -15,3 +19,9 @@ app.use("/payments", paymentsRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const initSql = fs.readFileSync(path.join(__dirname, "init.sql")).toString();
+
+db.query(initSql)
+  .then(() => console.log("✅ Tables checked/created"))
+  .catch((err) => console.error("❌ Init SQL error:", err));
