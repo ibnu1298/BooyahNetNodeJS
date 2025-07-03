b57d212d284492e56963dca161886a466574574c
Ibnu Aqil Al Barohin <kalangkang1298@gmail.com>
2025-07-03 07:20:38 +0700

    fixing user controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const users = require("../Controllers/UsersController");
+const users = require("../controllers/usersController");
 const verifyToken = require("../middleware/authMiddleware");
 const checkRole = require("../middleware/checkRole");
 
