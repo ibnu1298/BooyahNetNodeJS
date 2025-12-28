b6d0e0689ec90dbb4a1a06c4ee186f2f12bc7294
Ibnu Aqil Al Barohin <ibnuaqil.albarohin1298@gmail.com>
2025-12-29 06:27:38 +0700

    changes time interval

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ db.query(initSql)
   .catch((err) => console.error("❌ Init SQL error:", err));
 
 cron.schedule(
-  "*/10 * * * * *",
+  "0 7 * * *",
   () => {
     console.log(
       "🕐 Menjalankan job ReminderUser...",
