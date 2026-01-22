83c1379e129166e8c7a7923d2b83e205ebef3093
Ibnu Aqil Al Barohin <ibnuaqil.albarohin1298@gmail.com>
2026-01-22 16:38:04 +0700

    takeout wa

--- jobs/beforeBilling.js
+++ jobs/beforeBilling.js
@@ -25,23 +25,23 @@ module.exports = beforeBilling;
 async function reminder(data, days) {
   const filteredNextBilling = data.rows.filter(
     (row) =>
-      row.days_until_next_billing == days && Number(row.unpaid_payments) > 0
+      row.days_until_next_billing == days && Number(row.unpaid_payments) > 0,
   );
   const names = filteredNextBilling.map((row) => row.name || "-").join(", ");
   console.log(`${filteredNextBilling.length} User - (${names})`);
   for (const user of filteredNextBilling) {
     const message = await getMessageReminder(user.user_id, days);
     try {
-      await fetch(`${baseUrl}/send`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          to: user.phone,
-          message: message,
-        }),
-      });
+      // await fetch(`${baseUrl}/send`, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     to: user.phone,
+      //     message: message,
+      //   }),
+      // });
       console.log(`Successfully Send Reminder to ${user.name}`);
     } catch (error) {
       console.log(`Failed Send Reminder to ${user.name}, Error : ${error}`);
@@ -53,37 +53,37 @@ async function getMessageReminder(user_id, reminderDays) {
   let text = "";
   const { rows: users } = await pool.query(
     "SELECT * FROM users WHERE id = $1 ",
-    [user_id]
+    [user_id],
   );
   if (!users.length) return text;
 
   const { rows: payments } = await pool.query(
     "SELECT * FROM payments WHERE user_id = $1 AND is_paid = FALSE AND row_status = TRUE",
-    [user_id]
+    [user_id],
   );
   const { rows: nextPayments } = await pool.query(
     "SELECT * FROM payments WHERE user_id = $1 AND is_paid = FALSE AND billing_date_for >= CURRENT_DATE AND row_status = TRUE",
-    [user_id]
+    [user_id],
   );
   const { rows: latePayments } = await pool.query(
     "SELECT * FROM payments WHERE user_id = $1 AND is_paid = FALSE AND billing_date_for < CURRENT_DATE  AND row_status = TRUE",
-    [user_id]
+    [user_id],
   );
 
   const totalAmount = payments.reduce(
     (acc, item) => acc + Number(item.amount || 0),
-    0
+    0,
   );
 
   text = `*Hallo ${capitalizeString(users[0].name)}*\n`;
   if (nextPayments.length) {
     if (reminderDays > 0) {
       text += `Mengingatkan bahwa *${reminderDays} hari lagi* adalah tanggal pembayaran tagihan *WIFI BooyahNet*\npada hari ${formatBillingDateWithDay(
-        nextPayments[0].billing_date_for
+        nextPayments[0].billing_date_for,
       )} - Rp ${formatRupiah(nextPayments[0].amount)}`;
     } else {
       text += `Hari ini adalah tanggal pembayaran *WIFI BooyahNet* sebesar Rp ${formatRupiah(
-        nextPayments[0].amount
+        nextPayments[0].amount,
       )}`;
     }
   }
@@ -94,14 +94,14 @@ async function getMessageReminder(user_id, reminderDays) {
       text += `masih ada ${latePayments.length} tagihan yang belum di bayar\n\nBerikut tagihan nya:\n`;
       latePayments.forEach((item, index) => {
         text += `*• ${formatBillingDate(
-          item.billing_date_for
+          item.billing_date_for,
         )} :Rp ${formatRupiah(item.amount)}*\n`;
       });
     } else if (latePayments.length == 1) {
       text += `masih ada ${
         latePayments.length
       } tagihan yang belum di bayar di tanggal ${formatBillingDate(
-        latePayments[0].billing_date_for
+        latePayments[0].billing_date_for,
       )} - Rp ${formatRupiah(latePayments[0].amount)}\n`;
     }
   }
